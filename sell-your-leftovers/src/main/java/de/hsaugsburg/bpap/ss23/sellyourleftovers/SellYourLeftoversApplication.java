7a70fae5b81aec2bfacfb3007968ab6990df6402
Andreas Schmid <andreas.schmid1@hs-augsburg.de>
2023-02-14 14:13:12 +0100

    Initial commit

---
+++ sell-your-leftovers/src/main/java/de/hsaugsburg/bpap/ss23/sellyourleftovers/SellYourLeftoversApplication.java
@@ -0,0 +1,13 @@
+package de.hsaugsburg.bpap.ss23.sellyourleftovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellYourLeftoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellYourLeftoversApplication.class, args);
+	}
+
+}
---
+++ sell-your-leftovers/src/test/java/de/hsaugsburg/bpap/ss23/sellyourleftovers/SellYourLeftoversApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsaugsburg.bpap.ss23.sellyourleftovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellYourLeftoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
