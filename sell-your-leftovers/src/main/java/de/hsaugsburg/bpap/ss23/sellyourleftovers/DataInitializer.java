fe3d647fd9f711c6707f3468f97a3c870c87398b
Andreas Schmid <schmidandreas.ul@gmail.com>
2023-02-15 11:58:29 +0100

    Add test data within the DataInitializer

---
+++ sell-your-leftovers/src/main/java/de/hsaugsburg/bpap/ss23/sellyourleftovers/DataInitializer.java
@@ -0,0 +1,42 @@
+package de.hsaugsburg.bpap.ss23.sellyourleftovers;
+
+import de.hsaugsburg.bpap.ss23.sellyourleftovers.category.Category;
+import de.hsaugsburg.bpap.ss23.sellyourleftovers.category.CategoryRepository;
+import de.hsaugsburg.bpap.ss23.sellyourleftovers.category.CategoryService;
+import de.hsaugsburg.bpap.ss23.sellyourleftovers.product.Product;
+import de.hsaugsburg.bpap.ss23.sellyourleftovers.product.ProductRepository;
+import de.hsaugsburg.bpap.ss23.sellyourleftovers.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category beer = new Category(1L, Category.CategoryType.BEER, "Bier");
+        Category wine = new Category(2L, Category.CategoryType.WINE, "Wein");
+        Category spirits = new Category(3L, Category.CategoryType.SPIRITS, "Spirituosen");
+        Category nonAlcoholic = new Category(4L, Category.CategoryType.NONALCOHOLIC, "Nicht-alkoholische Getränke");
+
+
+
+        categoryRepository.save(beer);
+        categoryRepository.save(wine);
+        categoryRepository.save(spirits);
+        categoryRepository.save(nonAlcoholic);
+
+        productRepository.save(new Product(1L,"A1","Coca Cola", "0.5 Liter originale Coca Cola",new BigDecimal(2), 10,nonAlcoholic));
+        productRepository.save(new Product(2L,"B2","Riegele Urhell", "0.5 Liter originales Riegele Helles",new BigDecimal(1.7), 5, beer));
+        productRepository.save(new Product(3L,"C3","Sonnenhof Samtrot fruchtsüß","0.7 Liter Sonnenhofer Rotwein",new BigDecimal(4.99), 20,wine));
+        productRepository.save(new Product(4L,"D4","Absolut Vodka", "Hochprozentiger Wodka",new BigDecimal(9.99), 2,spirits));
+}
+}
